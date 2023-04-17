bb08d0dcac8e821e759a5700690d0963c3927958
chen-100 <63862901+chen-100@users.noreply.github.com>
2023-04-17 12:00:27 +0000

    finish

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -32,12 +31,14 @@ fn main() {
     );
 }
 
+use crate::List::{Cons,Nil};
 pub fn create_empty_list() -> List {
-    todo!()
+    Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let lis = Cons(1,Box::new(Cons(2,Box::new(Nil))));
+    lis
 }
 
 #[cfg(test)]
